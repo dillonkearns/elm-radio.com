c333a0632451924c33f4662b0df1b619cd1b7202
Dillon Kearns <dillon@dillonkearns.com>
2021-07-26 15:04:09 -0700

    Add sitemap.

--- src/Api.elm
+++ src/Api.elm
@@ -3,8 +3,10 @@ module Api exposing (routes)
 import ApiRoute
 import DataSource exposing (DataSource)
 import Html exposing (Html)
+import Path
 import PodcastFeed
 import Route exposing (Route)
+import Sitemap
 
 
 routes :
@@ -16,5 +18,26 @@ routes getStaticRoutes htmlToString =
       --, MySitemap.build { siteUrl = canonicalSiteUrl } siteMetadata |> Ok
       ApiRoute.succeed PodcastFeed.generate
         |> ApiRoute.literal "feed.xml"
+        |> ApiRoute.single
+    , ApiRoute.succeed
+        (getStaticRoutes
+            |> DataSource.map
+                (\allRoutes ->
+                    { body =
+                        allRoutes
+                            |> List.map
+                                (\route ->
+                                    { path =
+                                        route
+                                            |> Route.toPath
+                                            |> Path.toAbsolute
+                                    , lastMod = Nothing
+                                    }
+                                )
+                            |> Sitemap.build { siteUrl = "https://elm-radio.com" }
+                    }
+                )
+        )
+        |> ApiRoute.literal "sitemap.xml"
         |> ApiRoute.single
     ]

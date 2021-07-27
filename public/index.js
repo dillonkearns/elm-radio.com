ad1e3f84ce9ffdfee1d5ea1f2cc3a060851c1ce8
Dillon Kearns <dillon@dillonkearns.com>
2021-07-26 17:04:57 -0700

    Remove console log.

--- public/index.js
+++ public/index.js
@@ -3,8 +3,6 @@
 /** @type ElmPagesInit */
 export default {
   load: async function (elmLoaded) {
-    const app = await elmLoaded;
-    console.log("App loaded", app);
   },
   flags: function () {
     return "You can decode this in Shared.elm using Json.Decode.string!";

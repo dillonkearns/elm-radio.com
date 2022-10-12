8301f8876fe69228c82cfbd75a9c4497ca00636f
Dillon Kearns <dillon@incrementalelm.com>
2022-10-12 14:37:12 -0700

    Wire up tailwind through vite.

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-        './src/**/*.elm'
-    ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
+const cssnano = require("cssnano");
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./{app,src}/**/*.elm"],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
 
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        ...process.env.NODE_ENV === 'production'
-            ? [purgecss]
-            : []
-    ]
-}
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production"
+      ? cssnano({ preset: "default" })
+      : null,
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};

2d418b300fee9430886e384990e327b3adf7c8c3
Dillon Kearns <dillon@incrementalelm.com>
2022-10-28 06:48:47 +0530

    Fix clean script to only apply within transcripts folder.

--- clean-transcripts.sh
+++ clean-transcripts.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 # This command is for Mac (might need to tweak for other OSes): https://blog.jasonmeridth.com/posts/use-git-grep-to-replace-strings-in-files-in-your-git-repository/
-git grep -l 'Dylan' | xargs sed -i '' -e 's/Dylan/Dillon/g'
+git grep -l 'Dylan' -- transcripts/ | xargs sed -i '' -e 's/Dylan/Dillon/g'

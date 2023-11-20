3fdcd8cb48366e97902f750d5cece2227b99b306
Dillon Kearns <dillon@incrementalelm.com>
2023-11-20 09:47:01 -0800

    Update transcripts.

--- clean-transcripts.sh
+++ clean-transcripts.sh
@@ -2,3 +2,4 @@
 
 # This command is for Mac (might need to tweak for other OSes): https://blog.jasonmeridth.com/posts/use-git-grep-to-replace-strings-in-files-in-your-git-repository/
 git grep -l 'Dylan' -- transcripts/ | xargs sed -i '' -e 's/Dylan/Dillon/g'
+git grep -l 'Dyron' -- transcripts/ | xargs sed -i '' -e 's/Dyron/Dillon/g'
